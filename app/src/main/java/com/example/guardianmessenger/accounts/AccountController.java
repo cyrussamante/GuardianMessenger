e9ce5fe7a604e3ed28d5d72e1afa92c1f3aa682b
Andrew <andrewm8387@gmail.com>
2024-03-24 15:57:31 -0400

    Removed RegistraionController and added outline for AccountController

---
+++ app/src/main/java/com/example/guardianmessenger/accounts/AccountController.java
@@ -0,0 +1,25 @@
+package com.example.guardianmessenger.accounts;
+
+import com.example.guardianmessenger.employees.*;
+
+public class AccountController {
+
+    public AccountController() {
+        // Empty constructor
+    }
+
+    public EmployeeInfo getEmployeeInfo(String username) {
+        //TODO: Get employee info from the database
+        return new EmployeeInfo();
+    }
+
+    public void updateEmployeeInfo(EmployeeInfo employeeInfo) {
+        //TODO: Update employee info in the database
+    }
+
+    public void deleteAccount(Employee employee) {
+        //TODO: Delete account from the database
+    }
+
+
+}
