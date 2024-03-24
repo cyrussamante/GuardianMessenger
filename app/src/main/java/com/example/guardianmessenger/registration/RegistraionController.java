a2849db51c0a6d7e3778fdde2e53be7d7ec8ccc8
Andrew <andrewm8387@gmail.com>
2024-03-24 15:29:25 -0400

    Created Employee and EmployeeInfo data classes. Created an outine for the RegistraionController

---
+++ app/src/main/java/com/example/guardianmessenger/registration/RegistraionController.java
@@ -0,0 +1,28 @@
+package com.example.guardianmessenger.registration;
+
+import com.example.guardianmessenger.employees.*;
+
+public class RegistraionController {
+
+    public RegistraionController() {
+        // Constructor
+    }
+
+    public boolean verifyInfo(EmployeeInfo info) {
+        //TODO: Verify the information
+        return false;
+    }
+
+
+    public Employee createNewAccount(EmployeeInfo info) {
+        //TODO: Create a new account
+        return new Employee(-1, null, null, null);
+    }
+
+    public boolean isEmployee(Employee employee) {
+        //TODO: Check if the employee exists
+        return false;
+    }
+
+
+}
