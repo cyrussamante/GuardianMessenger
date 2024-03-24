87a0d2f5637400464844ffc89e860dbd81fbf7b4
Andrew <andrewm8387@gmail.com>
2024-03-24 17:13:33 -0400

    Added to AccountController

--- app/src/main/java/com/example/guardianmessenger/accounts/AccountController.java
+++ app/src/main/java/com/example/guardianmessenger/accounts/AccountController.java
@@ -1,20 +1,47 @@
 package com.example.guardianmessenger.accounts;
 
 import com.example.guardianmessenger.employees.*;
+import com.google.firebase.auth.*;
+import com.google.firebase.*;
+
 
 public class AccountController {
 
+    private FirebaseAuth mAuth;
     public AccountController() {
         // Empty constructor
     }
 
-    public EmployeeInfo getEmployeeInfo(String username) {
+    public EmployeeInfo getUserInfo() {
+        FirebaseUser user = mAuth.getCurrentUser();
+        assert user != null;
+        String name = user.getDisplayName();
+        String email = user.getEmail();
+        return new EmployeeInfo(name, email);
+    }
+
+    public EmployeeInfo getEmployeeInfo(Employee employee) {
         //TODO: Get employee info from the database
-        return new EmployeeInfo();
+        return new EmployeeInfo(null, null);
+    }
+
+    public void updateUserInfo(EmployeeInfo employeeInfo) {
+        FirebaseUser user = mAuth.getCurrentUser();
+        assert user != null;
+        user.updateEmail(employeeInfo.email);
+        user.updateProfile(new com.google.firebase.auth.UserProfileChangeRequest.Builder()
+                .setDisplayName(employeeInfo.name)
+                .build());
     }
 
     public void updateEmployeeInfo(EmployeeInfo employeeInfo) {
         //TODO: Update employee info in the database
+    }
+
+    public void deleteUser() {
+        FirebaseUser user = mAuth.getCurrentUser();
+        assert user != null;
+        user.delete();
     }
 
     public void deleteAccount(Employee employee) {
--- app/src/main/java/com/example/guardianmessenger/employees/EmployeeInfo.java
+++ app/src/main/java/com/example/guardianmessenger/employees/EmployeeInfo.java
@@ -1,4 +1,26 @@
 package com.example.guardianmessenger.employees;
 
 public class EmployeeInfo {
+
+    public String name;
+    public String email;
+    private int age;
+    public String position;
+    public String department;
+    private int salary;
+
+    public EmployeeInfo(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public EmployeeInfo(String name, String email, int age, String position, String department, int salary) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.position = position;
+        this.department = department;
+        this.salary = salary;
+    }
+
 }
